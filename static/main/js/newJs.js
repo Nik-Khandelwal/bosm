68832df34e589d501b27b8f4ec46013e66966ad0
niravasher <f20171066@pilani.bits-pilani.ac.in>
2019-03-19 22:45:04 +0530

    made front.html page

---
+++ static/main/js/newJs.js
@@ -0,0 +1,4 @@
+if (window.width < window.height) {
+    document.getElementsByClassName("stad")[0].style.display = "none";
+    document.getElementsByClassName("stadad")[0].style.display = "none";
+}
